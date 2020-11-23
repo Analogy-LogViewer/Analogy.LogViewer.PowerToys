1dcd7d6e7225365e565677720cf1d7f15632c441
Lior Banai <liorbanai@gmail.com>
2020-11-23 09:54:00 +0200

    disable regex for now

--- Analogy.LogViewer.PowerToys/IAnalogy/DataProvidersFactory.cs
+++ Analogy.LogViewer.PowerToys/IAnalogy/DataProvidersFactory.cs
@@ -8,6 +8,10 @@ namespace Analogy.LogViewer.PowerToys.IAnalogy
     {
         public override Guid FactoryId { get; set; } = PrimaryFactory.Id;
         public override string Title { get; set; } = "Log Parsers";
-        public override IEnumerable<IAnalogyDataProvider> DataProviders { get; set; } = new List<IAnalogyDataProvider> {new PowerToysRegexOfflineDataProvider(), new OfflineDataProvider() };
+        public override IEnumerable<IAnalogyDataProvider> DataProviders { get; set; } = new List<IAnalogyDataProvider>
+        {
+            //new PowerToysRegexOfflineDataProvider(), 
+            new OfflineDataProvider()
+        };
     }
 }
